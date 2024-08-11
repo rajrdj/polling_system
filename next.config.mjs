b9df9124da765f9dad3e5a64b5b4ae8f003376ab
rajrdj <rajdahayt74@gmail.com>
2024-08-12 00:16:23 +0530

    FG

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 export const reactStrictMode = true;
-export const distDir = 'build';
+
 export function webpack(config, { isServer }) {
     if (!isServer) {
         config.resolve.fallback = { fs: false, path: false };
